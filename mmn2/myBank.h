ccc1a4796c7f0e07fbb3cb46d5e4c5890db35a1a
liron <--global>
2019-11-28 22:33:33 +0200

    add

---
+++ mmn2/myBank.h
@@ -0,0 +1,21 @@
+
+// a constant definition 
+#define N 50
+#define M 2
+
+double a[N][M];     //declaration of array
+
+// a function prototype for a function exported by library:
+double twoDigits(double num);
+double calculateInterest(double num,double interest);
+int counter(int count) ;
+void openAndSave(int count);
+int checkAccount(int accountNumber);
+void putMoney(int accountNumber);
+void cashWithrawal(int accountNumber,double moneyWithdrawal);
+void closeAccount(int accountNumber);
+void addInterest(double interestRate);
+void printAccounts();
+void closeAllAccount();
+
+
